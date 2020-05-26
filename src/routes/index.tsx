db8c4ca27183fd08f3904b2f62a3885a32deb037
Diou Medeiros <diou.182@gmail.com>
2020-05-25 23:12:52 -0300

    feat: Created Routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import CarDetails from '../pages/CarDetails';
+
+const Auth = createStackNavigator();
+
+const AuthRoutes: React.FC = () => (
+  <Auth.Navigator
+    screenOptions={{
+      headerTintColor: '#fff',
+      headerStyle: {
+        backgroundColor: '#08A4BD',
+      },
+      cardStyle: { backgroundColor: '#f4f4f4' },
+    }}
+  >
+    <Auth.Screen name="CardDetails" component={CarDetails} />
+  </Auth.Navigator>
+);
+
+export default AuthRoutes;
