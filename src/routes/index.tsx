dcafd4a3335f0ef24261706d743c3d12ae794d2d
Diou Medeiros <diou.182@gmail.com>
2020-06-11 18:26:55 -0300

    feat: Created Drawer Navigation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import CarDetails from '../pages/CarDetails';
 import Attendance from '../pages/Attendance';
 
 const Auth = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 const AuthRoutes: React.FC = () => (
-  <Auth.Navigator
-    screenOptions={{
-      headerTintColor: '#fff',
-      headerTitleAlign: 'center',
-      headerStyle: {
-        backgroundColor: '#08A4BD',
-      },
-      cardStyle: { backgroundColor: '#f4f4f4' },
-    }}
-  >
-    <Auth.Screen name="Detalhes" component={CarDetails} />
-    <Auth.Screen name="Atendimentos" component={Attendance} />
-  </Auth.Navigator>
+  // <Auth.Navigator
+  //   screenOptions={{
+  //     headerTintColor: '#fff',
+  //     headerTitleAlign: 'center',
+  //     headerStyle: {
+  //       backgroundColor: '#08A4BD',
+  //     },
+  //     cardStyle: { backgroundColor: '#f4f4f4' },
+  //   }}
+  // >
+  //   <Auth.Screen name="Detalhes" component={CarDetails} />
+  //   <Auth.Screen name="Atendimentos" component={Attendance} />
+  // </Auth.Navigator>
+  <Drawer.Navigator initialRouteName="Detalhes">
+    <Drawer.Screen name="Detalhes" component={CarDetails} />
+    <Drawer.Screen name="Atendimentos" component={Attendance} />
+  </Drawer.Navigator>
 );
 
 export default AuthRoutes;
